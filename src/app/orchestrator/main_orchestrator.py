292f2a2463a8a400cff46bf59798ccd925bf4738
alexmagalhaesdev <summer.magalhaes@gmail.com>
2024-06-21 18:54:26 -0300

    orchestrator works

--- src/app/orchestrator/main_orchestrator.py
+++ src/app/orchestrator/main_orchestrator.py
@@ -1,3 +1,4 @@
+import os
 from loguru import logger
 from llama_index.core import (
     Settings,
@@ -5,22 +6,31 @@ from llama_index.core import (
     SimpleDirectoryReader,
     StorageContext,
 )
-from llama_index.llms.openai import OpenAI
+
 from llama_index.readers.file import PDFReader
+from llama_index.llms.replicate import Replicate
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.fastembed import FastEmbedEmbedding
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
 from db.vectordb_client import qdrant_client
 
+os.environ["REPLICATE_API_TOKEN"] = "r8_1iGzzmnDEtIIsfTtwnvedBtCWkWk7Vs2wspxG"
+
 try:
     # LLM Model global settings
     logger.info("Setting LLM Model Global Configuration ...")
-    Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
+    Settings.llm = Replicate(
+        model="meta/llama-2-70b-chat:2796ee9483c3fd7aa2e171d38f4ca12251a30609463dcfd4cd76703f22e96cdf",
+        is_chat_model=True,
+        additional_kwargs={"max_new_tokens": 512},
+    )
 
     # Embedding Model global settings
     logger.info("Setting Embedding Model Global Configuration ...")
-    Settings.embed_model = FastEmbedEmbedding(model_name="BAAI/bge-base-en-v1.5")
+    Settings.embed_model = FastEmbedEmbedding(
+        model_name="BAAI/bge-base-en-v1.5", max_length=384
+    )
 
     # Chunk Pattern Global Settings
     logger.info("Setting Chunk Pattern Global Configuration ...")
@@ -32,7 +42,8 @@ try:
     parser = PDFReader()
     file_extractor = {".pdf": parser}
     documents = SimpleDirectoryReader(
-        "./data", file_extractor=file_extractor
+        "/home/alex/Documents/myprojects/harveyIA/src/app/orchestrator/data",
+        file_extractor=file_extractor,
     ).load_data()
     logger.info(f"Loaded {len(documents)} documents")
 
@@ -40,7 +51,9 @@ try:
     logger.info(
         "Setting up QdrantVectorStore with client=qdrant_client, collection_name='teste' ..."
     )
-    vector_store = QdrantVectorStore(client=qdrant_client, collection_name="teste")
+    vector_store = QdrantVectorStore(
+        client=qdrant_client, collection_name="pdf_collection"
+    )
 
     # Storage context setup
     logger.info(
@@ -48,14 +61,14 @@ try:
     )
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
+    print(f"my storage context {storage_context}")
+
     # Create index using documents and storage context
     logger.info(
         "Creating VectorStoreIndex from loaded documents and StorageContext ..."
     )
     vector_index = VectorStoreIndex.from_documents(
-        documents=documents,
-        storage_context=storage_context,
-        transformations=[text_splitter],
+        documents=documents, storage_context=storage_context
     )
     logger.info("VectorStoreIndex created successfully")
 
