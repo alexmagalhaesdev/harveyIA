fa6fc62c5f6efe70af9d292ed8260c23ad3c80a0
alexmagalhaesdev <summer.magalhaes@gmail.com>
2024-06-22 17:42:57 -0300

    updated orchestrator class

--- src/app/orchestrator/orchestrator.py
+++ src/app/orchestrator/orchestrator.py
@@ -22,7 +22,7 @@ class PromptOrchestrator:
         )
         self.llm = None
 
-    def load_documents(self) -> list:
+    def load_documents(self):
         """Loads a PDF document and returns a list of its pages."""
         loader = PyPDFLoader(self.pdf_file_path)
         documents = loader.load()
@@ -35,39 +35,39 @@ class PromptOrchestrator:
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size, chunk_overlap=chunk_overlap
         )
-        document_chunks = text_splitter.split_documents(documents)
-        return document_chunks
+        return text_splitter.split_documents(documents)
 
-    def build_vector_store(self, document_chunks) -> Chroma:
-        """Builds the Chroma vector store and assigns it to self.vector_store."""
+    def build_vector_store(self):
+        """Builds and returns the Chroma vector store from the PDF document."""
+        documents = self.load_documents()
+        document_chunks = self.split_documents(documents)
         vector_store = Chroma.from_documents(document_chunks, self.embeddings)
         retriever = vector_store.as_retriever()
         return retriever
 
-    def retrieve_relevant_documents(
-        self, retriever: str, query: str, k: int = 3
-    ) -> list:
+    def retrieve_relevant_documents(self, retriever, query: str, k: int = 3) -> list:
         """Retrieves the k most relevant text chunks from the vector store based on a query."""
         context = retriever.similarity_search(query, k=k)
         return context
 
-    def generate_prompt_template(self, query: str, context: str):
+    def generate_prompt_template(self, query, context):
         prompt = hub.pull("rlm/rag-prompt")
         return prompt.format(question=query, context=context)
 
-    def create_rag_chain(context, prompt, llm):
+    def create_rag_chain(self, context, prompt):
         rag_chain = (
-            {"context": context | format_docs, "question": RunnablePassthrough()}
+            {"context": context | format_docs(), "question": RunnablePassthrough()}
             | prompt
-            | llm
+            | self.llm
             | StrOutputParser()
         )
         return rag_chain
 
-    def answer_query(self, rag_chain=None, query=None):
-        """Generates an answer to the query using the retrieved texts and an LLM."""
-        if rag_chain is None:
-            rag_chain = self.create_rag_chain(self.context, self.prompt, self.llm)
-
+    def answer_query(self, query: str) -> str:
+        """Generates an answer to the query using the RAG process."""
+        retriever = self.build_vector_store()
+        relevant_docs = self.retrieve_relevant_documents(retriever, query)
+        prompt_template = self.generate_prompt_template(query, relevant_docs)
+        rag_chain = self.create_rag_chain(relevant_docs, prompt_template)
         rag_response = rag_chain.invoke(query)
         return rag_response
